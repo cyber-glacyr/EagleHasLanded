cb9bc5b1de41673451e0c7e00d92490bfc7d6bdf
j-simoneaux <11johnsim18@gmail.com>
2024-09-28 22:41:06 -0500

    Defining Base Functions

--- app.py
+++ app.py
@@ -7,5 +7,30 @@ app = Flask(__name__)
 def main():
     return render_template('main.html', viability=is_viable())
 
+
+def convert_to_cords(address):
+    """
+    convert address into latitude / longitude
+    """
+    latitude = ''
+    longitude = ''
+    return latitude + ',' + longitude
+
+
 def is_viable():
-    return 'not viable'
+    """
+    - solar, price, and roofing
+    """
+
+
+def solar_viable(lat, long):
+    """
+    does the house get enough energy year-long?
+    """
+
+
+def price_viable(currentCost):
+    """
+    does the cost of solar outweigh the current cost of bills?
+    """
+    solarCost = 20000
